7a4b453a0078184d415c42d7ee60c5c55941ae49
Lyndon <lyndonhebditch@gmail.com>
2025-02-23 15:03:58 +0000

    fix: add prefix to s3 bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,13 +3,15 @@ import { Construct } from "constructs";
 
 import { RemovalPolicy } from "aws-cdk-lib";
 
+const RESOURCE_PREFIX = "sograt";
+
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // S3 bucket for artifacts
     new s3.Bucket(this, "artifacts-bucket", {
-      bucketName: `artifacts-${scope.node.addr}`,
+      bucketName: `${RESOURCE_PREFIX}-artifacts-${scope.node.addr}`,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,

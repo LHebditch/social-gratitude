8f2fd2848f50e73f131c7107fb22adef77306507
Lyndon Hebditch <lyndonhebditch@gmail.com>
2025-02-23 15:05:05 +0000

    fix: update bucket name

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 ## prerequisites
 
-Before deploying anything you need to have set up an AWS account and inside that set up the OIDC role `GithubActionsAssumeRole` that grants github access to full access to cloudformation.
+Before deploying anything you need to have set up an AWS account and inside that set up the OIDC role `GithubActionsAssumeRole` that grants:
+
+- full access to cloudformation
+- read only access to ssm
+- read only access to s3
+- ability to assume and pass the cdk roles created by bootstrap step (deploy and file-publishing)
+
+You will also need to run the `cdk bootstrap` command in the desired acount to allow aws cdk to make changes
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,14 +1,17 @@
-import { Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib";
+import { Names, Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import { RemovalPolicy } from "aws-cdk-lib";
+
+const RESOURCE_PREFIX = "sograt";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // S3 bucket for future use
-    new s3.Bucket(this, "social-gratitude-bucket", {
+    // S3 bucket for artifacts
+    new s3.Bucket(this, "artifacts-bucket", {
+      bucketName: `${RESOURCE_PREFIX}-artifacts-${scope.node.addr}`,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,

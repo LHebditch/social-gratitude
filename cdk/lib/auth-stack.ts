57d985f7c7146cc4135c3d4141335b2f4704d0d9
Lyndon <lyndonhebditch@gmail.com>
2025-02-24 13:25:22 +0000

    feat(api): add auth api gateway

--- cdk/lib/auth-stack.ts
+++ cdk/lib/auth-stack.ts
@@ -6,12 +6,17 @@ import {
     aws_lambda_nodejs as lambda,
     aws_dynamodb as db,
     aws_logs as logs,
+    aws_apigatewayv2 as apigwv2,
+    aws_apigatewayv2_integrations,
 } from "aws-cdk-lib"
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+
+const { HttpLambdaIntegration } = aws_apigatewayv2_integrations;
 
 export const BuildAuthStack = (scope: Stack) => {
     const stack = new NestedStack(scope, "auth-stack");
 
+    // DYNAMO TABLE //
     const tableName = 'auth-table';
     const props: db.TableProps = {
         tableName,
@@ -26,10 +31,10 @@ export const BuildAuthStack = (scope: Stack) => {
         billingMode: db.BillingMode.PAY_PER_REQUEST,
         timeToLiveAttribute: '_ttl',
     };
-
     const table = new db.Table(stack, tableName, props);
 
-    const registerLambda = new lambda.NodejsFunction(stack, 'register-new-user-function', {
+    // FUNCTIONS //
+    const registerFn = new lambda.NodejsFunction(stack, 'register-new-user-function', {
         runtime: Runtime.NODEJS_22_X,
         handler: "index.handler",
         functionName: `auth-register-${stack.node.addr}`,
@@ -38,11 +43,33 @@ export const BuildAuthStack = (scope: Stack) => {
             AUTH_TABLE_NAME: table.tableName,
         },
         timeout: Duration.millis(3000),
-    })
+    });
 
-    addLogGRoup(stack, "auth-register-function", registerLambda)
-    table.grantReadWriteData(registerLambda);
-}
+    addLogGRoup(stack, "auth-register-function", registerFn);
+    table.grantReadWriteData(registerFn);
+
+    // API //
+    const corsOptions = {
+        allowMethods: [
+            apigwv2.CorsHttpMethod.GET,
+            apigwv2.CorsHttpMethod.HEAD,
+            apigwv2.CorsHttpMethod.OPTIONS,
+            apigwv2.CorsHttpMethod.POST,
+        ],
+        allowOrigins: ['*'],
+        maxAge: Duration.days(10),
+    };
+    const authApi = new apigwv2.HttpApi(stack, "auth-api", {
+        corsPreflight: corsOptions,
+    });
+
+    // API Routes //
+    authApi.addRoutes({
+        path: '/register',
+        methods: [apigwv2.HttpMethod.POST],
+        integration: new HttpLambdaIntegration("auth-register-function", registerFn),
+    });
+};
 
 const addLogGRoup = (stack: Stack, name: string, lambda: lambda.NodejsFunction) => {
     new logs.LogGroup(stack, `${name}-loggroup`, {
@@ -50,4 +77,4 @@ const addLogGRoup = (stack: Stack, name: string, lambda: lambda.NodejsFunction) 
         retention: logs.RetentionDays.THREE_MONTHS,
         removalPolicy: RemovalPolicy.DESTROY
     });
-}
+};
